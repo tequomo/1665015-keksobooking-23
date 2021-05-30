998e2163bf2072caac2f2e1e7cd2d910a6402ccb
tequomo <tequomo@gmail.com>
2021-05-30 13:31:14 +0300

    ~

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
+function getRandomIntInclusive(min, max) { // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function getRandomInteger(minValue, maxValue) {
-  // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
 
   minValue = (minValue < 0) ? Math.abs(minValue) : minValue;
   maxValue = (maxValue < 0) ? Math.abs(maxValue) : maxValue;
@@ -23,7 +23,6 @@ function getRandomInteger(minValue, maxValue) {
 }
 
 getRandomInteger(8, 15);
-
 
 function getRandomFloat(minValue, maxValue, decimal) {
 
