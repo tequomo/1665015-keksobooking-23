2f40f87312fb576f4fd39b28421e3fc2ab20e0cb
tequomo <tequomo@gmail.com>
2021-06-17 04:19:34 +0300

    Добавил функцию генерации объявлений

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 import './util.js';
-import { getOffersArray, OFFERS_COUNT } from './data.js';
-import { generateCard } from './card.js';
-
-const offers = getOffersArray(OFFERS_COUNT);
-
-offers;
-
-generateCard();
+import './data.js';
+import './card.js';
